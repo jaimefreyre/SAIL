5bd4958860a67e18ba81476a352efdd453484fd5
Budapest <estudiobudapest@gmail.com>
2021-12-06 17:20:25 -0300

    V1

--- src/app/auth/helpers/jwt.interceptor.ts
+++ src/app/auth/helpers/jwt.interceptor.ts
@@ -21,11 +21,11 @@ export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const currentUser = this._authenticationService.currentUserValue;
     const isLoggedIn = currentUser && currentUser.token;
-    console.log('Se activa Interceptor JWT para ingrear Token: ' + currentUser.token);
+    // console.log('Se activa Interceptor JWT para ingrear Token: ' + currentUser.token);
     const isApiUrl = request.url.startsWith(environment.apiUrl);  
     if (isLoggedIn && isApiUrl) {
-      console.log('paso Login');
-      console.log(currentUser.token);
+      // console.log('paso Login');
+      // console.log(currentUser.token);
       request = request.clone({
         setHeaders: {
           // Authorization: `Bearer ${currentUser.token}`
