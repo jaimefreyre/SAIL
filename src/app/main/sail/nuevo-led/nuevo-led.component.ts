6357817f2a0e7965997218275ac43f061a14f562
Budapest <estudiobudapest@gmail.com>
2021-12-23 17:06:38 -0300

    J

--- src/app/main/sail/nuevo-led/nuevo-led.component.ts
+++ src/app/main/sail/nuevo-led/nuevo-led.component.ts
@@ -23,27 +23,12 @@ export class NuevoLedComponent implements OnInit {
   public landmarkVar;
   public addressVar;
 
- 
+  //Variable Score
+  public score;
 
-  public selectBasic = [
-    { name: 'UK' },
-    { name: 'USA' },
-    { name: 'Spain' },
-    { name: 'France' },
-    { name: 'Italy' },
-    { name: 'Australia' }
-  ];
-
+  //Select
   public selectMulti = [{ name: 'English' }, { name: 'French' }, { name: 'Spanish' }];
   public selectMultiSelected;
-
-  // private
-  private horizontalWizardStepper: Stepper;
-  private verticalWizardStepper: Stepper;
-  private modernWizardStepper: Stepper;
-  private modernVerticalWizardStepper: Stepper;
-  private bsStepper;
-
 
 
   /**
@@ -52,59 +37,6 @@ export class NuevoLedComponent implements OnInit {
    */
   public tp1Form = 0;
 
-  /**
-   * Horizontal Wizard Stepper Next
-   *
-   * @param data
-   */
-  horizontalWizardStepperNext(data) {
-    if (data.form.valid === true) {
-      this.horizontalWizardStepper.next();
-    }
-  }
-  /**
-   * Horizontal Wizard Stepper Previous
-   */
-  horizontalWizardStepperPrevious() {
-    this.horizontalWizardStepper.previous();
-  }
-
-  /**
-   * Vertical Wizard Stepper Next
-   */
-  verticalWizardNext() {
-    this.verticalWizardStepper.next();
-  }
-  /**
-   * Vertical Wizard Stepper Previous
-   */
-  verticalWizardPrevious() {
-    this.verticalWizardStepper.previous();
-  }
-  /**
-   * Modern Horizontal Wizard Stepper Next
-   */
-  modernHorizontalNext() {
-    this.modernWizardStepper.next();
-  }
-  /**
-   * Modern Horizontal Wizard Stepper Previous
-   */
-  modernHorizontalPrevious() {
-    this.modernWizardStepper.previous();
-  }
-  /**
-   * Modern Vertical Wizard Stepper Next
-   */
-  modernVerticalNext() {
-    this.modernVerticalWizardStepper.next();
-  }
-  /**
-   * Modern Vertical Wizard Stepper Previous
-   */
-  modernVerticalPrevious() {
-    this.modernVerticalWizardStepper.previous();
-  }
 
   /**
    * On Submit
@@ -129,49 +61,7 @@ export class NuevoLedComponent implements OnInit {
    * On Init
    */
   ngOnInit() {
-    this.horizontalWizardStepper = new Stepper(document.querySelector('#stepper1'), {});
-
-    this.verticalWizardStepper = new Stepper(document.querySelector('#stepper2'), {
-      linear: false,
-      animation: true
-    });
-
-    this.modernWizardStepper = new Stepper(document.querySelector('#stepper3'), {
-      linear: false,
-      animation: true
-    });
-
-    this.modernVerticalWizardStepper = new Stepper(document.querySelector('#stepper4'), {
-      linear: false,
-      animation: true
-    });
-
-    this.bsStepper = document.querySelectorAll('.bs-stepper');
-
-    // content header
-    this.contentHeader = {
-      headerTitle: 'Form Wizard',
-      actionButton: true,
-      breadcrumb: {
-        type: '',
-        links: [
-          {
-            name: 'Home',
-            isLink: true,
-            link: '/'
-          },
-          {
-            name: 'Forms',
-            isLink: true,
-            link: '/'
-          },
-          {
-            name: 'Form Wizard',
-            isLink: false
-          }
-        ]
-      }
-    };
+   
 
   }
 
